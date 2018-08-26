4e9a554d44d452412605a651722a12dd269025a1
Jesus Morales Jr <jmjles@hotmail.com>
2018-08-26 03:45:24 -0700

    Minor Update

--- js/ko.js
+++ js/ko.js
@@ -1,29 +1,28 @@
-$("#movie a").on("mouseover click", function(){
-	$('.intro').css("background-color", "#A26A42");
+$(function () {
+    $("#movie a").on("mouseover click", function () {
+        $('.intro').css("background-color", "#A26A42");
+    });
+    $("#movie a").on("mouseout", function () {
+        $('.intro').css("background-color", "#f9f9ea");
+    });
+    $("#youtube a").on("mouseover click", function () {
+        $('.intro').css("background-color", "red");
+    });
+    $("#youtube a").on("mouseout", function () {
+        $('.intro').css("background-color", "#f9f9ea");
+    });
+    $("#github a").on("mouseover click", function () {
+        $('.intro').css("background-color", "grey");
+    });
+    $("#github a").on("mouseout", function () {
+        $('.intro').css("background-color", "#f9f9ea");
+    });
 });
-$("#movie a").on("mouseout", function(){
-	$('.intro').css("background-color", "#f9f9ea");
-});
-
-$("#youtube a").on("mouseover click", function(){
-	$('.intro').css("background-color", "red");
-});
-$("#youtube a").on("mouseout", function(){
-	$('.intro').css("background-color", "#f9f9ea");
-});
-
-$("#github a").on("mouseover click", function(){
-	$('.intro').css("background-color", "grey");
-});
-$("#github a").on("mouseout", function(){
-	$('.intro').css("background-color", "#f9f9ea");
-});
-
-$(function(){
+$(function () {
 	$("#slides").slidesjs({
-		width:100,
-		height:300,
-		play:{
+		width: 100,
+		height: 300,
+		play: {
 			active: true,
 			effect: "slide",
 			interval: 3000,
